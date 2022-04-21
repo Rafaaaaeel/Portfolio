c99528c0795be805e9df27262ef1db5c47a65250
Rafael Oliveira <rafaelopires@icloud.com>
2022-04-21 00:00:30 -0300

    portfolio done

--- src/js/popup-funtion.js
+++ src/js/popup-funtion.js
@@ -1,13 +1,17 @@
 const opoyPopupScreen = document.querySelector(".popup-screen-opoy")
 const bankeyPopupScreen = document.querySelector(".popup-screen-bankey")
+const menuPopUpScreen = document.querySelector(".popup-screen-menu")
 
-const closebtn = document.querySelector(".close-popup")
+const closebtnOpoy = document.querySelector(".close-popup-opoy")
+const closebtnBankey = document.querySelector(".close-popup-bankey")
+const closebtnMenu = document.querySelector(".close-popup-menu")
 
 const popupCookie = document.cookie.valueOf("popUpShow")
 
 
 const bankeyPopUp = document.querySelector("#bankey")
 const opoyPopUp = document.querySelector("#opoy")
+const menuPopUp = document.querySelector("#menu")
 
 opoyPopUp.addEventListener("click", ()=>{
     opoyPopupScreen.classList.add("popupActive") 
@@ -17,11 +21,21 @@ bankeyPopUp.addEventListener("click", ()=>{
     bankeyPopupScreen.classList.add("popupActive") 
 })
 
+menuPopUp.addEventListener("click", ()=>{
+    menuPopUpScreen.classList.add("popupActive") 
+})
 
 
 
 
-closebtn.addEventListener("click", ()=>{
-    bankeyPopupScreen.classList.remove("popupActive")
+closebtnOpoy.addEventListener("click", ()=>{
     opoyPopupScreen.classList.remove("popupActive")
+})
+
+closebtnBankey.addEventListener("click", ()=>{
+    bankeyPopupScreen.classList.remove("popupActive")
+})
+
+closebtnMenu.addEventListener("click", ()=>{
+    menuPopUpScreen.classList.remove("popupActive")
 })
